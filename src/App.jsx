1ef1fdc2cc76eac8c110ef56cff04937bbf2889c
is-Lew <lewis.halder@outlook.com>
2022-02-22 10:31:05 +0000

    thrown my 2 cents in

--- src/App.jsx
+++ src/App.jsx
@@ -7,6 +7,7 @@ function App() {
       <div>Ollie woz ere 2k22</div>
       <div>Ebrima was here</div>
       <p>I am unhappy with the lack of testing - matt</p>
+      <p>something funny</p>
     </div>
   );
 }
