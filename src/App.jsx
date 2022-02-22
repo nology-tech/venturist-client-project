6f1505ef4a260a0a9dc0afcbca7eacff9a2d91b0
Sean Pursani <seanpursani@gmail.com>
2022-02-22 11:43:10 +0100

    Added my tag

--- src/App.jsx
+++ src/App.jsx
@@ -3,9 +3,8 @@ import './App.scss';
 function App() {
   return (
     <div className="App">
-
       <p>Hello,it's Lana</p>
-
+      <div>Sean woz ere</div>
       <div>Ollie woz ere 2k22</div>
       <div>Happi is being happy :)</div>
       <div>Ebrima was here</div>
