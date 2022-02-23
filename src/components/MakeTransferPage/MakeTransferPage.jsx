cf2ae4fe7f17ae69d6c5859cf126ca1bf192086e
Sean Pursani <seanpursani@gmail.com>
2022-02-23 17:56:28 +0100

    added confirmdetails component with a base structure

--- src/components/MakeTransferPage/MakeTransferPage.jsx
+++ src/components/MakeTransferPage/MakeTransferPage.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import "./MakeTransferPage.scss";
 import MakeTransferForm from "../MakeTransferForm/MakeTransferForm";
 import AddRecipient from '../AddRecipient/AddRecipient';
+import ConfirmDetails from '../ConfirmDetails/ConfirmDetails';
 
 const MakeTransferPage = () => {
 
@@ -11,7 +12,7 @@ const MakeTransferPage = () => {
         <h6>Transfer</h6>
         <h1>Make Transfer</h1>
         <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cursus nibh sit eu sagittis. Integer amet, donec massa fermentum nunc eget netus.</p>
-        <AddRecipient />
+        <ConfirmDetails />
     </div>
 
   )
