85158d8de7a98cf6263e868d1b02c0fec9fb1a6e
Ommote <olivia.motevalli@gmail.com>
2022-02-23 15:59:53 +0000

    created variables & assets directories, installed fontawesome icons, added flag pngs

---
+++ src/assets/icons/icons.js
@@ -0,0 +1,9 @@
+const icons = [
+  { Convert: <FontAwesomeIcon icon="fa-solid fa-arrows-rotate" /> },
+  { Wallet: <FontAwesomeIcon icon="fa-solid fa-wallet" /> },
+  { LiveRates: <FontAwesomeIcon icon="fa-solid fa-chart-line" /> },
+  { Transfer: <FontAwesomeIcon icon="fa-solid fa-paper-plane" /> },
+  { Contacts: <FontAwesomeIcon icon="fa-solid fa-user-group" /> },
+];
+
+// Upon saving, double quotes around convert, wallet, live rates, transfer and contacts were removed.
