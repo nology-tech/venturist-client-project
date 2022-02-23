d72ebd9a0b1ebb4f960634c0bd77ccab74754082
William Busby <williambusby1999@gmail.com>
2022-02-23 16:10:51 +0000

    Added imports for scss files

--- src/components/MakeTransferChooseRecipient/MakeTransferChooseRecipient.jsx
+++ src/components/MakeTransferChooseRecipient/MakeTransferChooseRecipient.jsx
@@ -1,4 +1,5 @@
-import React from 'react'
+import React from 'react';
+import "./MakeTransferChooseRecipient.scss";
 
 const MakeTransferChooseRecipient = (props) => {
   return (
--- src/components/MakeTransferRecipientCard/MakeTransferRecipientCard.jsx
+++ src/components/MakeTransferRecipientCard/MakeTransferRecipientCard.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import "./MakeTransferChooseRecipientCard.scss";
 
 const MakeTransferRecipientCard = (props) => {
 
