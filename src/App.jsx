ed7fc825cca33862265184c8f326c53962387c91
Allan Shrestha <stevevai111@hotmail.com>
2022-02-23 15:04:46 +0000

    New changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,9 @@
 import "./App.scss";
 
-function App() {
+const App = () => {
   return (
     <div className="App">
-      <section className="intro">
-        <div>Howsit?</div>
-        <div>p tags are the best</div>
-        <div>Other stuff</div>
-        <div>Hello,it's Lana</div>
-        <div>Ollie woz ere 2k22</div>
-        <div>I like this!</div>
-        <div>PLEASE DONT MESS UP HG</div>
-        <div>William was also here</div>
-        <div>Happi is being happy :)</div>
-        <div>Ebrima was here</div>
-        <div>I am unhappy with the lack of testing - matt</div>
-        <div>something funny</div>
-      </section>
+      
     </div>
   );
 }
---
+++ src/containers/navbar /Navbar.jsx
@@ -0,0 +1,13 @@
+import React, {useState} from "react";
+import './Navbar.scss';
+
+const navbar = () => {
+  
+  return (
+    <div>
+
+    </div>
+  )
+}
+
+export default navbar
