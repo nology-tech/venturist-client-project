2fef58524a797b744b56a3aa4ac3dba7b5b5226d
William Busby <williambusby1999@gmail.com>
2022-02-23 16:37:45 +0000

    Added choose recipient pop up main HTML

--- src/components/MakeTransferChooseRecipient/MakeTransferChooseRecipient.jsx
+++ src/components/MakeTransferChooseRecipient/MakeTransferChooseRecipient.jsx
@@ -2,8 +2,26 @@ import React from 'react';
 import "./MakeTransferChooseRecipient.scss";
 
 const MakeTransferChooseRecipient = (props) => {
+
+  const {userCardList} = props;
+
+  const userCardComponents = userCardList.map(userCard => <MakeTransferChooseRecipientCard userCard={userCard} />)
+
   return (
-    <div>MakeTransferChooseRecipient</div>
+    <div className="transfer-page__choose-recipient">
+      <h2>Choose Recipient</h2>
+      <img src="close-cross" />
+      <p>Search</p>
+      <img src="magnifying glass" alt="Search" />
+      <input type="text" placeholder="Search..."/>
+
+      <div className="transfer-page__recipient-list">
+        <p>Name</p>
+        <p>Sort Code</p>
+        <p>Account No</p>
+        {userCardComponents}
+      </div>
+    </div>
   )
 }
 
