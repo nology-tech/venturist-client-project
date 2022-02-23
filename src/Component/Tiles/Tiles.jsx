70c699fb1e8f5168c4abe98292889a417b916e9d
Ebrima Sallah <sallahebrima@ymail.com>
2022-02-23 16:38:05 +0000

    created title and headers for welcome tiles

---
+++ src/Component/Tiles/Tiles.jsx
@@ -0,0 +1,19 @@
+import "./Tiles.scss";
+
+const Tiles= () => {
+
+
+  return (
+
+    <div className="tiles">
+      <h1>Hey Samantha</h1>
+      <div className="tiles__fund-box">
+        <h3 className="tiles__fund-box__header" >Your funds:</h3>
+        <h1 className="tiles__fund-box__amount">£12000</h1>
+        <div className="tiles__fund-box__semicicrle" ></div>
+      </div>
+    </div>
+  );
+}
+
+export default Tiles;
