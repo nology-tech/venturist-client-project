d7f33068e6b094d09aaf0aa9ef20ca1945ee65c1
William Busby <williambusby1999@gmail.com>
2022-02-23 16:10:13 +0000

    Added content for each card

---
+++ src/components/MakeTransferRecipientCard/MakeTransferRecipientCard.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const MakeTransferRecipientCard = (props) => {
+
+  const { userCard } = props;
+
+  return (
+    <div className="transfer-page__recipient__card">
+      <img src={userCard.bankImage} alt={userCard.bankName}></img>
+      <p>{userCard.personName}</p>
+      <p>{userCard.sortCode}</p>
+      <p>{userCard.accountNumber}</p>
+    </div>
+  )
+}
+
+export default MakeTransferRecipientCard
