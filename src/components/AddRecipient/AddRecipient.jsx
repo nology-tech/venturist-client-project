379e2081c305697e48818ce4db83a4044f23f405
Sean Pursani <seanpursani@gmail.com>
2022-02-23 17:36:18 +0100

    Frame for Addrecipient component

---
+++ src/components/AddRecipient/AddRecipient.jsx
@@ -0,0 +1,34 @@
+import React from 'react'
+import "./AddRecipient.scss"
+
+const AddRecipient = () => {
+  return (
+    <div className='transfer-page__add-recipient'>
+      <h2>Add Recipient</h2>
+      <form onSubmit="handleSubmit">
+        <div>
+          <h4>Recipient Name</h4>
+          <input type="text" name="" id="" />
+        </div>
+        <div>
+          <h4>Account Type</h4>
+          <input type="text" name="" id="" />
+        </div>
+        <div>
+          <h4>Account Number</h4>
+          <input type="text" name="" id="" />
+        </div>
+        <div>
+          <h4>Sort Code</h4>
+          <input type="text" name="" id="" />
+        </div>
+        <div>
+          <p>Cancel</p>
+          <button type="submit">Continue</button>
+        </div>
+      </form>
+    </div>
+  )
+}
+
+export default AddRecipient
--- src/components/MakeTransferForm/MakeTransferForm.jsx
+++ src/components/MakeTransferForm/MakeTransferForm.jsx
@@ -4,42 +4,42 @@ import "./MakeTransferForm.scss"
 const MakeTransferForm = (props) => {
 
   const {
-    exchangeRateSend,
-    exchangeRateRecieve,
-    exchangeFee,
-    exchangeDeliveryTime
+    // exchangeRateSend,
+    // exchangeRateRecieve,
+    // exchangeFee,
+    // exchangeDeliveryTime
   } = props;
 
   return (
-    <form className='transfer-page__transfer-form' onSubmit="">
+    <form className='transfer-page__transfer-form' onSubmit="handleSubmit">
       <div className="">
         <h4>You send</h4>
         <div>
-          <p>{exchangeRateSend.currencyFlag}</p>
+          {/* <p>{exchangeRateSend.currencyFlag}</p>
           <p>{exchangeRateSend.currencyCode} - {exchangeRateSend.currencyName}</p>
-          <p>{exchangeRateSend.currencySymbol}</p>
-          <input type="number" />
+          <p>{exchangeRateSend.currencySymbol}</p> */}
+          <input type="text" />
         </div>
         <h4>Recipient gets</h4>
         <div>
-          <p>{exchangeRateRecieve.currencyFlag}</p>
+          {/* <p>{exchangeRateRecieve.currencyFlag}</p>
           <p>{exchangeRateRecieve.currencyCode} - {exchangeRateRecieve.currencyName}</p>
-          <p>{exchangeRateRecieve.currencySymbol}</p>
-          <input type="number" />
+          <p>{exchangeRateRecieve.currencySymbol}</p> */}
+          <input type="text" />
         </div>
       </div>
       <div>
         <div>
           <p>Rate</p>
-          <p>{exchangeRateRecieve.liveRate}</p>
+          {/* <p>{exchangeRateRecieve.liveRate}</p> */}
         </div>
         <div>
           <p>Fee</p>
-          <p>{exchangeFee}</p>
+          {/* <p>{exchangeFee}</p> */}
         </div>
         <div>
           <p>Delivery</p>
-          <p>{exchangeDeliveryTime}</p>
+          {/* <p>{exchangeDeliveryTime}</p> */}
         </div>
       </div>
       <div>
