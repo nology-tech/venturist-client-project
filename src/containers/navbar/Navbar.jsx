7e042aa65e0565117a433552dba579c12e8d97b4
mattser <jomatser@gmail.com>
2022-02-23 15:01:34 +0000

    Started Navbar Structure

---
+++ src/containers/navbar/Navbar.jsx
@@ -0,0 +1,27 @@
+import React, {useState} from "react";
+// import './Navbar.scss';
+
+const Navbar = () => {
+
+  const [activeButton,setActiveButton] = useState();
+
+
+  return (
+     // Logo
+
+    // Couple of Buttons
+
+    // Bunch of Empty Space
+
+    // Sign Out Button
+    <nav>
+
+    
+    
+
+    </nav>
+    
+  )
+}
+
+export default Navbar;
