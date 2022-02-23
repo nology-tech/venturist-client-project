9964cea62e34d70217104e37e4fe242d6ca25b92
William Busby <williambusby1999@gmail.com>
2022-02-23 16:09:33 +0000

    Added choose recipient files

---
+++ src/components/MakeTransferChooseRecipient/MakeTransferChooseRecipient.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const MakeTransferChooseRecipient = (props) => {
+  return (
+    <div>MakeTransferChooseRecipient</div>
+  )
+}
+
+export default MakeTransferChooseRecipient
