8ec42c75a005aecab12beaeb438486cc6a0a3608
mattser <jomatser@gmail.com>
2022-02-23 15:38:31 +0000

    Made Nav Items

---
+++ src/components/NavItem/NavItem.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const NavItem = (props) => {
+  let className = 'navbar__nav-item';
+  if (props.activeButton === props.label) {
+    className += "navbar__nav-item--active"
+  }
+  return (
+    <div className={className} onClick={() => props.handleClick(props.label)}>
+      <div className='navbar__nav-item--icon'></div>
+      <h3>{props.label}</h3>
+    </div>
+  )
+}
+
+export default NavItem
--- src/containers/navbar/Navbar.jsx
+++ src/containers/navbar/Navbar.jsx
@@ -1,9 +1,19 @@
 import React, {useState} from "react";
+import NavItem from "../../components/NavItem/NavItem";
 // import './Navbar.scss';
 
 const Navbar = () => {
 
   const [activeButton,setActiveButton] = useState();
+
+  const handleClick = (label) => {
+    setActiveButton(label);
+  }
+
+  const buttonLabels = ["Wallet","Live Rates","Convert","Transfer","Contacts"];
+
+  const buttons = buttonLabels.map(label => <NavItem label={label} handleClick={handleClick} activeButton={activeButton}/>)
+
 
 
   return (
@@ -16,8 +26,9 @@ const Navbar = () => {
     // Sign Out Button
     <nav>
 
-    
-    
+
+      {buttons}
+      <p>{activeButton}</p>
 
     </nav>
     
