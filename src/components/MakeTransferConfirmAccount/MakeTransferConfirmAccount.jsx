84e586da0b8f7c92771bba4ab0bc0dadc2c4a90a
William Busby <williambusby1999@gmail.com>
2022-02-23 16:09:20 +0000

    Added content to confirm account used page

---
+++ src/components/MakeTransferConfirmAccount/MakeTransferConfirmAccount.jsx
@@ -0,0 +1,35 @@
+import React from 'react';
+import "./MakeTransferConfirm.scss";
+
+const MakeTransferConfirm = (props) => {
+
+  const { accountDetails, transferAmount } = props;
+
+  return (
+    <div className="make-transfer__confirm">
+      <h3>Send From</h3>
+      <div className="grey-background">
+        <h5 className="blue-colouring">Samantha Brooks</h5>
+        <div className="account-details">
+          <p>Account Number:</p>
+          <p>{accountDetails.accountNumber}</p>
+          <p>Sort Code:</p>
+          <p>{accountDetails.sortCode}</p>
+        </div>
+        <div className="total">
+          <h5>Total</h5>
+          <h5>{transferAmount}</h5>
+          <p>Funds Remaining:</p>
+          <p>{accountDetails.balance}</p>
+        </div>
+      </div>
+      <h3>To</h3>
+      <div className="make-transfer__confirm__recipient">
+        <button className="blue-background">Select Recipient</button>
+        <button className="white-background">+ Pay Someone New</button>
+      </div>
+    </div>
+  )
+}
+
+export default MakeTransferConfirm
