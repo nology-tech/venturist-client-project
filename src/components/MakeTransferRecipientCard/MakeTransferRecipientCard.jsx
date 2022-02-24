766c783c3e2b30e5014a536b562f9badc075dbed
William Busby <williambusby1999@gmail.com>
2022-02-24 16:56:16 +0000

    Started styling card - still positioning to do

--- src/components/MakeTransferRecipientCard/MakeTransferRecipientCard.jsx
+++ src/components/MakeTransferRecipientCard/MakeTransferRecipientCard.jsx
@@ -1,16 +1,25 @@
 import React from 'react'
-import "./MakeTransferChooseRecipientCard.scss";
+import "./MakeTransferRecipientCard.scss";
 
 const MakeTransferRecipientCard = (props) => {
 
-  const { userCard } = props;
+  // const { userCard } = props;
 
   return (
     <div className="transfer-page__recipient__card">
-      <img src={userCard.bankImage} alt={userCard.bankName}></img>
+      <div className="transfer-page__recipient__card--left">
+        <p className="bank-image">image</p>
+        <p>name</p>
+      </div>
+      <div className="transfer-page__recipient__card--right">
+        <p className="sort-code">sortCode</p>
+        <p className="account-number">accountNumber</p>
+      </div>
+
+      {/* <img src={userCard.bankImage} alt={userCard.bankName} className="bank-image"></img>
       <p>{userCard.personName}</p>
       <p>{userCard.sortCode}</p>
-      <p>{userCard.accountNumber}</p>
+      <p>{userCard.accountNumber}</p> */}
     </div>
   )
 }
