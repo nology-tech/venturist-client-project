c7513bf0590bc013ce9210a32004f43e6b342ce9
William Busby <williambusby1999@gmail.com>
2022-02-24 16:37:51 +0000

    Confirm account styling mostly completed.

--- src/components/MakeTransferConfirmAccount/MakeTransferConfirmAccount.jsx
+++ src/components/MakeTransferConfirmAccount/MakeTransferConfirmAccount.jsx
@@ -1,31 +1,45 @@
 import React from 'react';
-import "./MakeTransferConfirm.scss";
+import "./MakeTransferConfirmAccount.scss";
 
 const MakeTransferConfirmAccount = (props) => {
 
-  const { accountDetails, transferAmount } = props;
+  // const { accountDetails, transferAmount } = props;
+  const {transferAmount} = props;
 
   return (
-    <div className="transfer-page__confirm">
+    <div className="transfer-page__confirm grey-background">
       <h3>Send From</h3>
-      <div className="grey-background">
+      <div className="dark-grey-background account-details-full">
         <h5 className="blue-colouring">Samantha Brooks</h5>
         <div className="account-details">
-          <p>Account Number:</p>
-          <p>{accountDetails.accountNumber}</p>
-          <p>Sort Code:</p>
-          <p>{accountDetails.sortCode}</p>
+          <div className="account-number split-between">
+            <p>Account Number:</p>
+            {/* <p>{accountDetails.accountNumber}</p> */}
+            <p>10840366</p>
+          </div>
+          <div className="sort-code split-between">
+            <p>Sort Code:</p>
+            <p>110053</p>
+            {/* <p>{accountDetails.sortCode}</p> */}
+          </div>
         </div>
         <div className="total">
-          <h5>Total</h5>
-          <h5>{transferAmount}</h5>
-          <p>Funds Remaining:</p>
-          <p>{accountDetails.balance}</p>
+          <div className="split-between">
+            <h5>Total</h5>
+            <h5>{transferAmount}</h5>
+          </div>
+          <div className="balance split-between">
+            <p className="sub-text">Funds Remaining:</p>
+            {/* <p>{accountDetails.balance}</p> */}
+            <p>100</p>
+          </div>
         </div>
       </div>
       <h3>To</h3>
       <div className="transfer-page__confirm__recipient">
-        <button className="blue-background">Select Recipient</button>
+        <div className="middle-divider">
+          <button className="blue-background">Select Recipient</button>
+        </div>
         <button className="white-background">+ Pay Someone New</button>
       </div>
     </div>
