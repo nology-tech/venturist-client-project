aea1493449dae9ac93ac5cc1e5acfc5965b08964
Happi Kaur <38865414+happikaur@users.noreply.github.com>
2022-02-25 16:18:40 +0000

    Added some icons. Imported fonts from google font.

--- src/assets/icons/icons.js
+++ src/assets/icons/icons.js
@@ -6,6 +6,11 @@ const icons = [
   { Contacts: <FontAwesomeIcon icon="fa-solid fa-user-group" /> },
   { SignOut: <FontAwesomeIcon icon="fa-solid fa-arrow-right-from-bracket" /> },
   { dropDownProfile: <FontAwesomeIcon icon="fa-solid fa-chevron-down" /> },
+  { eyeShow: <FontAwesomeIcon icon="fa-regular fa-eye" /> },
+  { eyeHide: <FontAwesomeIcon icon="fa-regular fa-eye-slash" /> },
+  { tickSuccessful: <FontAwesomeIcon icon="fa-regular fa-check" /> },
+  { exclamation: <FontAwesomeIcon icon="fa-solid fa-exclamation" /> },
+  { search: <FontAwesomeIcon icon="fa-solid fa-magnifying-glass" /> },
 ];
 
 // Upon saving, double quotes around convert, wallet, live rates, transfer and contacts were removed.
