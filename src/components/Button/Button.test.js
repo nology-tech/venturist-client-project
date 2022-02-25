fada27d80b8a734a14b07a2aacce0b1042cc5694
= <=>
2022-02-25 16:04:11 +0000

    write test to check that a function passed in as a prop actually gets called

--- src/components/Button/Button.test.js
+++ src/components/Button/Button.test.js
@@ -1,4 +1,4 @@
-import { render, screen } from "@testing-library/react";
+import { fireEvent, render, screen } from "@testing-library/react";
 import Button from "./Button";
 
 test("Test that button renders with correct inner text", () => {
@@ -9,4 +9,15 @@ test("Test that button renders with correct inner text", () => {
   //Assert
   expect(button).toBeInTheDocument();
   expect(button).toHaveTextContent("test button");
+});
+
+test("Test that the button onClick={buttonFunction} calls the function passed in a prop", () => {
+  //Arrange
+  const onClick = jest.fn();
+  render(<Button buttonFunction={onClick} />);
+  const button = screen.getByTestId("button");
+  //Act
+  fireEvent.click(button);
+  //Assert
+  expect(onClick).toHaveBeenCalled();
 });
