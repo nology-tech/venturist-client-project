644b20b1fa4c0c803623303b89f472be2bf02457
mattser <jomatser@gmail.com>
2022-02-25 16:12:14 +0000

    Added symbols for William c:

--- src/assets/data/liveRatesExample.js
+++ src/assets/data/liveRatesExample.js
@@ -1,73 +1,85 @@
 
 const liveRatesArr = [
   {
-    currencyName = "British Pound",
-    currencyFlag = "url",
-    currencyCode = "GBP",
-    liveRate = 1.0000,
-    changeOfRate = 0.000
+    currencyName: "British Pound",
+    currencyFlag: "url",
+    currencyCode: "GBP",
+    liveRate: 1.0000,
+    changeOfRate: 0.000,
+    currencySymbol: "£" 
   },
   {
-    currencyName = "US Dollar",
-    currencyFlag = "url",
-    currencyCode = "USD",
-    liveRate = 1.3513,
-    changeOfRate = 0.004
+    currencyName: "US Dollar",
+    currencyFlag: "url",
+    currencyCode: "USD",
+    liveRate: 1.3513,
+    changeOfRate: 0.004,
+    currencySymbol: "$"
   },
   {
-    currencyName = "Euro",
-    currencyFlag = "url",
-    currencyCode = "EUR",
-    liveRate = 1.1423,
-    changeOfRate = -0.002
+    currencyName: "Euro",
+    currencyFlag: "url",
+    currencyCode: "EUR",
+    liveRate: 1.1423,
+    changeOfRate: -0.002,
+    currencySymbol: "€"
   },
   {
-    currencyName = "Swiss Franc",
-    currencyFlag = "url",
-    currencyCode = "CHF",
-    liveRate = 1.5367,
-    changeOfRate = 0.024
+    currencyName: "Swiss Franc",
+    currencyFlag: "url",
+    currencyCode: "CHF",
+    liveRate: 1.5367,
+    changeOfRate: 0.024,
+    currencySymbol: "₣"
   },
   {
-    currencyName = "Japanese Yen",
-    currencyFlag = "url",
-    currencyCode = "JPY",
-    liveRate = 150.4231,
-    changeOfRate = 0.102
+    currencyName: "Japanese Yen",
+    currencyFlag: "url",
+    currencyCode: "JPY",
+    liveRate: 150.4231,
+    changeOfRate: 0.102,
+    currencySymbol: "¥",
   },
   {
-    currencyName = "Korean Won",
-    currencyFlag = "url",
-    currencyCode = "KRW",
-    liveRate = 200.2305,
-    changeOfRate = 0.350
+    currencyName: "Korean Won",
+    currencyFlag: "url",
+    currencyCode: "KRW",
+    liveRate: 200.2305,
+    changeOfRate: 0.350,
+    currencySymbol: "₩"
   },
   {
-    currencyName = "Norweigan Kroner",
-    currencyFlag = "url",
-    currencyCode = "NOK",
-    liveRate = 1.1423,
-    changeOfRate = -0.002
+    currencyName: "Norweigan Kroner",
+    currencyFlag: "url",
+    currencyCode: "NOK",
+    liveRate: 1.1423,
+    changeOfRate: -0.002,
+    currencySymbol: "Kr."
+
   },
   {
-    currencyName = "Australian Dollar",
-    currencyFlag = "url",
-    currencyCode = "AUD",
-    liveRate = 1.4123,
-    changeOfRate = 0.310
+    currencyName: "Australian Dollar",
+    currencyFlag: "url",
+    currencyCode: "AUD",
+    liveRate: 1.4123,
+    changeOfRate: 0.310,
+    currencySymbol: "AUD$"
+    
   },
   {
-    currencyName = "Indian Rupee",
-    currencyFlag = "url",
-    currencyCode = "INR",
-    liveRate = 140.5314,
-    changeOfRate = -0.022
+    currencyName: "Indian Rupee",
+    currencyFlag: "url",
+    currencyCode: "INR",
+    liveRate: 140.5314,
+    changeOfRate: -0.022,
+    currencySymbol: "₹"
   },
   {
-    currencyName = "Chinese Yuan",
-    currencyFlag = "url",
-    currencyCode = "CNY",
-    liveRate = 140.5367,
-    changeOfRate = 0.173
+    currencyName: "Chinese Yuan",
+    currencyFlag: "url",
+    currencyCode: "CNY",
+    liveRate: 140.5367,
+    changeOfRate: 0.173,
+    currencyCode: "¥"
   },
 ]
