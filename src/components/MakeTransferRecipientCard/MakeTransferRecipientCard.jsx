82d21e40eb1a17b71713e3c3d2bd060389553aef
William Busby <williambusby1999@gmail.com>
2022-02-28 14:53:22 +0000

    Updated styling on card and using props inputs

--- src/components/MakeTransferRecipientCard/MakeTransferRecipientCard.jsx
+++ src/components/MakeTransferRecipientCard/MakeTransferRecipientCard.jsx
@@ -3,21 +3,25 @@ import "./MakeTransferRecipientCard.scss";
 
 const MakeTransferRecipientCard = (props) => {
 
-  // const { userCard } = props;
+  const { userCard } = props;
+
+  let personName;
+
+  if(userCard.middleNames) {
+    personName = userCard.firstName + " " + userCard.middleNames + " " + userCard.lastName;
+  } else {
+    personName = userCard.firstName + " " + userCard.lastName;
+  }
 
   return (
     <div className="transfer-page__recipient__card">
       <div className="transfer-page__recipient__card--left">
-        <p className="transfer-page__recipient__card__bank-image">image</p>
-        <p>name</p>
-        {/* <img src={userCard.bankImage} alt={userCard.bankName} className="bank-image"></img>
-        <p>{userCard.personName}</p> */}
+        <img src={userCard.bankIcon} alt={userCard.bankName} className="transfer-page__recipient__card__bank-image"></img>
+        <p>{personName}</p>
       </div>
       <div className="transfer-page__recipient__card--right">
-        <p className="transfer-page__recipient__card__sort-code">100154</p>
-        <p className="transfer-page__recipient__card__account-number">27385019</p>
-        {/* <p className="transfer-page__recipient__card__sort-code">{userCard.sortCode}</p>
-        <p className="transfer-page__recipient__card__account-number">{userCard.accountNumber}</p> */}
+        <p className="transfer-page__recipient__card__sort-code">{userCard.sortCode}</p>
+        <p className="transfer-page__recipient__card__account-number">{userCard.accountNumber}</p>
       </div>      
     </div>
   )
