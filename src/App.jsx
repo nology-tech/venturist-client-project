4616d517f02b5a0adad67164e7ff9ffbb347beac
mattser <jomatser@gmail.com>
2022-02-28 15:01:30 +0000

    Added routes to make build happy

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,21 @@
 import "./App.scss";
 import NavBar from "./containers/NavBar/NavBar";
-import { BrowserRouter as Router } from "react-router-dom";
+import { BrowserRouter as Router , Routes, Route } from "react-router-dom";
 
 const App = () => {
   return (
     <div className="App">
       <Router>
         <NavBar />
+        <Routes>
+          <Route path="/Wallet"/>
+          <Route path="/LiveRates"/>
+          <Route path="/Convert" />
+          <Route path="/Transfer" />
+          <Route path="/Contacts" />
+          <Route path="/Deposit" />
+          <Route path="/Withdraw" />
+        </Routes>
         
       </Router>
     </div>
