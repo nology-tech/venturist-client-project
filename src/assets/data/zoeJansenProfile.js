902e1a58eedcb42c5efd22b336039e5399e932fb
Ebrima Sallah <sallahebrima@ymail.com>
2022-02-28 16:16:56 +0000

    files updated

---
+++ src/assets/data/zoeJansenProfile.js
@@ -0,0 +1,23 @@
+const userProfile = {
+
+  firstName: "Zoe",
+  middleNames: "",
+  lastName: "Jansen",
+  accountNumber: 12345689,
+  sortCode: "553456",
+  IBAN: "GB56HLFX11005310847788",
+  cards = [
+    {
+      type: "VISA DEBIT",
+      accountType: "Current Account",
+      cardNumber: 4916006636410203,
+      amount: 1753.50,
+      currencyType: "GBP"
+    },
+  ],
+  holdings = {
+    EUR: 1753.50
+  }
+}
+
+export default userProfile;
