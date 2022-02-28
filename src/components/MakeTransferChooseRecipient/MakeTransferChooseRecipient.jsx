a47b313ce9e22b5528f69d4386f7e9b62e15b212
William Busby <williambusby1999@gmail.com>
2022-02-28 15:59:42 +0000

    Started styling recipients modal

--- src/components/MakeTransferChooseRecipient/MakeTransferChooseRecipient.jsx
+++ src/components/MakeTransferChooseRecipient/MakeTransferChooseRecipient.jsx
@@ -14,11 +14,11 @@ const MakeTransferChooseRecipient = (props) => {
 
   return (
     <div className="transfer-page__choose-recipient">
-      <h2>Choose Recipient</h2>
-      <img src="close-cross" alt="Close menu" />
-      <h4>Search</h4>
-      <img src="magnifying glass" alt="Search" />
-      <input type="text" placeholder="Search..."/>
+      <h2 className="transfer-page__choose-recipient__header">Choose Recipient</h2>
+      {/* <img src="close-cross" alt="Close menu" className="transfer-page__choose-recipient__close-menu"/> */}
+      <p className="transfer-page__choose-recipient__close-menu">X</p>
+      <h4 className="transfer-page__choose-recipient__subheader">Search</h4>
+      <input type="text" placeholder="Search..." className="transfer-page__choose-recipient__search"/>
 
       <div className="transfer-page__choose-recipient__recipient-list">
         <div className="transfer-page__choose-recipient__recipient-list__headers">
