e10e061f0e3110176562a43039b2b056108088a4
William Busby <williambusby1999@gmail.com>
2022-02-28 15:34:08 +0000

    Recipient list styling completed

--- src/components/MakeTransferChooseRecipient/MakeTransferChooseRecipient.jsx
+++ src/components/MakeTransferChooseRecipient/MakeTransferChooseRecipient.jsx
@@ -5,28 +5,30 @@ import "./MakeTransferChooseRecipient.scss";
 const MakeTransferChooseRecipient = (props) => {
 
   const {userCardList} = props;
+
+  let userCardListUsed = (userCardList.length !== 0);
   
   const userCardComponents = userCardList.map((userCard,index) => <MakeTransferRecipientCard key={index} userCard={userCard} />);
 
-  const emptyContactListText = <p>You don't seem to have any contacts yet, try adding one!</p>
+  const emptyContactListText = <p>You don't seem to have any contacts yet, try adding one!</p>;
 
   return (
     <div className="transfer-page__choose-recipient">
       <h2>Choose Recipient</h2>
-      <img src="close-cross" />
-      <p>Search</p>
+      <img src="close-cross" alt="Close menu" />
+      <h4>Search</h4>
       <img src="magnifying glass" alt="Search" />
       <input type="text" placeholder="Search..."/>
 
-      <div className="transfer-page__recipient-list">
-        <div className="transfer-page__recipient-list__headers">
-          <p>Name</p>
-          <p>Sort Code</p>
-          <p>Account No</p>
+      <div className="transfer-page__choose-recipient__recipient-list">
+        <div className="transfer-page__choose-recipient__recipient-list__headers">
+          <h5>Name</h5>
+          <h5 className="transfer-page__choose-recipient__recipient-list__headers__sort-code">Sort Code</h5>
+          <h5 className="transfer-page__choose-recipient__recipient-list__headers__account-no">Account No</h5>
         </div>
-        <div className="transfer-page__recipient-list__cards">
-          {!userCardList && emptyContactListText}
-          {userCardList && userCardComponents}
+        <div className="transfer-page__choose-recipient__recipient-list__cards">
+          {!userCardListUsed && emptyContactListText}
+          {userCardListUsed && userCardComponents}
         </div>
       </div>
     </div>
