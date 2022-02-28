2c46366e57e566a7f2fad9649515e4c7687d50f2
mattser <jomatser@gmail.com>
2022-02-28 16:39:22 +0000

    Added Icon to html and moved Components to components

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,12 @@
 import "./App.scss";
 import NavBar from "./containers/NavBar/NavBar";
 
-<<<<<<< HEAD
 const App = () => {
   return (
     <div className="App">
       
       <NavBar />
         
-=======
-
-function App() {
-  return (
-    <div className="App">
- 
-      <section className="intro">
-        <div className="currency-flag currency-flag-usd"></div>
-        <div>Howsit?</div>
-        <div>p tags are the best</div>
-        <div>Other stuff</div>
-        <div>Hello,it's Lana</div>
-        <div>Ollie woz ere 2k22</div>
-        <div>I like this!</div>
-        <div>PLEASE DONT MESS UP HG</div>
-        <div>William was also here</div>
-        <div>Happi is being happy :)</div>
-        <div>Ebrima was here</div>
-        <div>I am unhappy with the lack of testing - matt</div>
-        <div>something funny</div>
-      </section>
->>>>>>> 5f636024caee9b53bc3e06ff50cd653aea0392de
     </div>
   );
 }
