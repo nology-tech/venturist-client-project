bc4ffd73071df560001bb10580a171ccc52cd801
Lana Ferrari <ferrarirangel@gmail.com>
2022-03-01 15:46:47 +0000

    SCSS and HTML

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,14 @@
 import "./App.scss";
+import UserProfile from "./components/UserProfile/UserProfile";
 import NavBar from "./containers/NavBar/NavBar";
-
 
 const App = () => {
   return (
     <div className="App">
-      <NavBar /> 
+      <NavBar />
+      <UserProfile />
     </div>
   );
-}
+};
 
 export default App;
