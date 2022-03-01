3d9bc97430952ae22ca2a08be2448d32195f39c2
Allan Shrestha <stevevai111@hotmail.com>
2022-03-01 17:04:10 +0000

    Updated user profile

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,10 @@
 import "./App.scss";
-import UserProfile from "./components/UserProfile/UserProfile";
 import NavBar from "./containers/NavBar/NavBar";
 
 const App = () => {
   return (
     <div className="App">
       <NavBar />
-      <UserProfile />
     </div>
   );
 };
