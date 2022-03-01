5bb1d69f974eef0c0bfa5f318f9a46b68af0d10b
Happi Kaur <38865414+happikaur@users.noreply.github.com>
2022-03-01 16:10:01 +0000

    Made some test to check if it has any errors. Deleted the sample files.

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -6,10 +6,12 @@ const Header = (props) => {
   const { title, pageFunctionHeading, textDescription } = props;
 
   return (
-    <div className="header">
-      <h5 className="header__title">{title}</h5>
-      <h1 className="header__page-function">{pageFunctionHeading}</h1>
-      <p className="header__description">{textDescription}</p>
+    <div className="header" data-testid="header">
+      <h5 className="header__title" data-testid="title">{title}</h5>
+      <h1 className="header__page-function" data-testid="page-function"
+>{pageFunctionHeading}</h1>
+      <p className="header__description" data-testid="description"
+>{textDescription}</p>
     </div>
   );
 };
--- src/components/Header/Header.test.js
+++ src/components/Header/Header.test.js
@@ -1,21 +1,86 @@
-import { getAllByTestId, render,screen } from "@testing-library/react";
+import { getByTestId, render, screen } from "@testing-library/react";
 import Header from "./Header";
 
 describe("Testing the Header", () => {
-
-  test("Check to see if the Header renders", () => {
+  test("Check to see if Header Renders", () => {
     // Arrange
-    render(<Header />)
+    render(<Header />);
 
     // Act
-    const header = screen.get
+    const header = screen.getByTestId("header");
+    // Assert
+    expect(header).toBeInTheDocument();
+  });
+
+  //   Title (h5) test
+  test("Check if the Header title Renders", () => {
+    // Arrange
+    render(<Header />);
+
+    // Act
+    const title = screen.getByTestId("title");
 
     // Assert
-  })
-})
+    expect(title).toBeInTheDocument();
+  });
 
+  // Title - convert
+  test("Check to see if title 'convert' Renders", () => {
+    // Arrange
+    render(<Header title="Convert" />);
 
-// for h5
-// for h1
-// for p tag
-// if all shows at the same time
+    // Act
+    const title = screen.getByTestId("title");
+
+    // Assert
+    expect(title).toHaveTextContent("Convert");
+  });
+
+  //   Page function heading (h1) test
+  test("Check if the Header pageFunctionHeading Renders", () => {
+    // Arrange
+    render(<Header />);
+
+    // Act
+    const pageFunction = screen.getByTestId("page-function");
+
+    // Assert
+    expect(pageFunction).toBeInTheDocument();
+  });
+
+  //   Page function heading (h1) - currency converter
+  test("Check if the Header pageFunctionHeading 'currency converter' Renders", () => {
+    // Arrange
+    render(<Header pageFunctionHeading="Currency Converter"/>);
+
+    // Act
+    const pageFunction = screen.getByTestId("page-function");
+
+    // Assert
+    expect(pageFunction).toHaveTextContent("Currency Converter");
+  });
+
+  //   Description text (p tag) test
+  test("Check if the Header textDescription Renders", () => {
+    // Arrange
+    render(<Header />);
+
+    // Act
+    const textDescription = screen.getByTestId("description");
+
+    // Assert
+    expect(textDescription).toBeInTheDocument();
+  });
+
+  //   Description text (p tag) test - text
+  test("Check if the Header textDescription 'text' Renders", () => {
+    // Arrange
+    render(<Header textDescription="All your friends and family"/>);
+
+    // Act
+    const textDescription = screen.getByTestId("description");
+
+    // Assert
+    expect(textDescription).toHaveTextContent("All your friends and family");
+  });
+});
