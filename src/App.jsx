b1f72ca0911f97040668782cecf98e6085becb0c
is-Lew <lewis.halder@outlook.com>
2022-03-01 15:51:40 +0000

    grey background on contact box

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,15 @@
 import "./App.scss";
+import MakeTransferChooseRecipient from "./components/MakeTransferChooseRecipient/MakeTransferChooseRecipient";
 import NavBar from "./containers/NavBar/NavBar";
+import Data from "./assets/data/contactExample";
 
 const App = () => {
   return (
     <div className="App">
+
       
       <NavBar />
+      
         
     </div>
   );
