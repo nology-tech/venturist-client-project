980092b33b7a29b61fbbac3a568fe2a3c053229d
Happi Kaur <38865414+happikaur@users.noreply.github.com>
2022-03-01 16:31:44 +0000

    Cleaned the app.jsx file

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,10 @@
 import "./App.scss";
-import Currency from "./components/Currency/Currency";
-import TransferPage from "./components/TransferPage/TransferPage";
 
 function App() {
 
   return (
     <div className="App">
-      <TransferPage />
-      <Currency />
-      {/* <Header h5={"Convert"} h1={"Currency Converter"}/> */}
-      <section className="intro">
-        <div className="currency-flag currency-flag-usd"></div>
-      </section>
+     
     </div>
   );
 }
