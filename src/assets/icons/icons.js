c031e0e5d8b795d0b54b5d1ddea2355064b7ac60
Ebrima Sallah <sallahebrima@ymail.com>
2022-03-01 15:42:28 +0000

    merge

--- src/assets/icons/icons.js
+++ src/assets/icons/icons.js
@@ -1,19 +1,3 @@
-<<<<<<< HEAD
-const icons = [
-  { Convert: <FontAwesomeIcon icon="fa-solid fa-arrows-rotate" /> },
-  { Wallet: <FontAwesomeIcon icon="fa-solid fa-wallet" /> },
-  { LiveRates: <FontAwesomeIcon icon="fa-solid fa-chart-line" /> },
-  { Transfer: <FontAwesomeIcon icon="fa-solid fa-paper-plane" /> },
-  { Contacts: <FontAwesomeIcon icon="fa-solid fa-user-group" /> },
-  { SignOut: <FontAwesomeIcon icon="fa-solid fa-arrow-right-from-bracket" /> },
-  { dropDownProfile: <FontAwesomeIcon icon="fa-solid fa-chevron-down" /> },
-  { eyeShow: <FontAwesomeIcon icon="fa-regular fa-eye" /> },
-  { eyeHide: <FontAwesomeIcon icon="fa-regular fa-eye-slash" /> },
-  { tickSuccessful: <FontAwesomeIcon icon="fa-regular fa-check" /> },
-  { exclamation: <FontAwesomeIcon icon="fa-solid fa-exclamation" /> },
-  { search: <FontAwesomeIcon icon="fa-solid fa-magnifying-glass" /> },
-];
-=======
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
 
@@ -36,6 +20,5 @@ const icons = {
 };
 
 export default icons;
->>>>>>> cb5143fbb2a56cdc2f875354ccd290299489c598
 
 // Upon saving, double quotes around convert, wallet, live rates, transfer and contacts were removed.
