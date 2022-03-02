68b9bf61c4df5652380cd1e50af7663b2f380c97
Ebrima Sallah <sallahebrima@ymail.com>
2022-03-02 15:28:43 +0000

    added a dropdown file and created a map function to pass down options as props

--- src/containers/CurrencyConverter/CurrencyConverter.jsx
+++ src/containers/CurrencyConverter/CurrencyConverter.jsx
@@ -1,10 +1,14 @@
 import React from 'react'
-import "./CurrencyConverter.scss"
+import DropDown from '../../components/DropDown/DropDown'
+import "./CurrencyConverter.scss";
+
+const testOptions = ["GBP","USD","EUR","YEN","AUD"];
 
 const CurrencyConverter = (props) => {
+
   return (
     <section className="currency-converter">
-      
+      <DropDown options={testOptions}/>
     </section>
   )
 }
