3d0153768c0553dc42bd0ce1f6d07a9a01c805b0
Naib Rahman <naib.rahman@gmail.com>
2022-03-02 14:27:59 +0000

    add header

--- src/containers/ConvertPage/ConvertPage.jsx
+++ src/containers/ConvertPage/ConvertPage.jsx
@@ -1,17 +1,20 @@
-import React from 'react'
+import React, {useState, useEffect} from 'react'
 import "./ConvertPage.scss";
 import userProfile from "../../assets/data/samanthaBrooksProfile";
 import liveRatesArr from "../../assets/data/liveRatesExample";
-
-
+import Header from '../../components/Header/Header'
 
 const ConvertPage = () => {
-  console.log(userProfile);
-  console.log(liveRatesArr);
-  <p>live rates</p>
 
+  useEffect(() => {},userProfile,liveRatesArr)
+
+  const [profile,setProfile] = useState({...userProfile});
+  const [rates,setRates] = useState([...liveRatesArr]);
+  
   return (
-    <div>ConvertPage</div>
+    <section className='convert-page'>
+      <Header title="Convert" pageFunctionHeading="Currency Converter" textDescription="Buy and exchange currencies with ease" />
+    </section>
   )
 }
 
