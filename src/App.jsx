30995b491e703a6e082a99e82dfcfccacaa79eed
is-Lew <lewis.halder@outlook.com>
2022-03-02 17:04:25 +0000

    cleaned up app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,12 @@
 import "./App.scss";
-// import MakeTransferChooseRecipient from "./components/MakeTransferChooseRecipient/MakeTransferChooseRecipient";
-import MakeTransferPage from "./components/MakeTransferPage/MakeTransferPage";
-// import MakeTransferRecipientCard from './components/MakeTransferRecipientCard/MakeTransferRecipientCard';
 import NavBar from "./containers/NavBar/NavBar";
-import MakeTransferForm from "./components/MakeTransferForm/MakeTransferForm";
 
 
 const App = () => {
   return (
     <div className="App">
       <NavBar /> 
-      <MakeTransferForm />
+    
     </div>
   );
 }
