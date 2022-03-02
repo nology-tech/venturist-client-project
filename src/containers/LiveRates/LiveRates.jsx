b308b0f403833b777cdab72d902fac955aebef2c
= <=>
2022-03-02 15:04:30 +0000

    add boilerplate for liveRates table and component

---
+++ src/containers/LiveRates/LiveRates.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+const LiveRates = () => {
+  return (
+    <table>
+      <tr>
+        <th>Currency</th>
+        <th>Amount</th>
+        <th>Rate</th>
+      </tr>
+    </table>
+  );
+};
+
+export default LiveRates;
