ea1d18b0b9303444c5afdc7540c08e4c1d21e081
Ebrima Sallah <sallahebrima@ymail.com>
2022-03-03 16:04:00 +0000

    Props passed down from convert page

--- src/containers/ConvertPage/ConvertPage.jsx
+++ src/containers/ConvertPage/ConvertPage.jsx
@@ -3,18 +3,24 @@ import "./ConvertPage.scss";
 // import userProfile from "../../assets/data/samanthaBrooksProfile";
 // import liveRatesArr from "../../assets/data/liveRatesExample";
 import Header from '../../components/Header/Header'
+import DropDown from '../../components/DropDown/DropDown';
+const testoptions = ["GBP", "YEN", "USD"];
 
 const ConvertPage = () => {
 
+
+ 
   // useEffect(() => {},userProfile,liveRatesArr)
 
   // const [profile,setProfile] = useState({...userProfile});
   // const [rates,setRates] = useState([...liveRatesArr]);
+  
 
   
   return (
     <section className='convert-page'>
       <Header title="Convert" pageFunctionHeading="Currency Converter" textDescription="Buy and exchange currencies with ease" />
+      <DropDown options={testoptions} />
     </section>
   )
 }
