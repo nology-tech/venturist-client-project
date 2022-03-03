f8306f9b4fd09807b79e7cb21ce66d3306e94aec
= <=>
2022-03-03 15:52:02 +0000

    add render method for filtering out base currency

--- src/containers/LiveRates/LiveRates.jsx
+++ src/containers/LiveRates/LiveRates.jsx
@@ -3,21 +3,25 @@ import LiveRatesItem from "../../components/LiveRatesItem/LiveRatesItem";
 
 const LiveRates = (props) => {
   const { rates } = props;
-  // flagImg, currency, amount, rate, sendFunction, buttonName
 
-  /*
-    {
-    currencyName: "British Pound",
-    currencyFlag: "../../assets/countryFlags/gbp.png",
-    currencyCode: "GBP",
-    liveRate: 1.0000,
-    changeOfRate: 0.000,
-    currencySymbol: "£" 
-  }
-  */
   const [baseCurrency, setBaseCurrency] = useState("GBP");
 
-  const renderBaseCurrency = () => {};
+  const renderBaseCurrency = () => {
+    return rates
+      .filter((currency) => currency.currencyCode === baseCurrency)
+      .map((item, index) => {
+        return (
+          <LiveRatesItem
+            key={index}
+            flagImg={item.currencyFlag}
+            currency={item.currencyCode}
+            amount={item.liveRate}
+            rate={item.changeOfRate}
+            buttonName="Edit"
+          />
+        );
+      });
+  };
 
   const renderList = () => {
     return rates.map((currency, index) => {
@@ -37,8 +41,6 @@ const LiveRates = (props) => {
     });
   };
 
-  console.log(rates);
-
   return (
     <table>
       <tr>
@@ -46,6 +48,7 @@ const LiveRates = (props) => {
         <th>Amount</th>
         <th>Rate</th>
       </tr>
+      {renderBaseCurrency()}
       {renderList()}
     </table>
   );
