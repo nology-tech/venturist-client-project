8f8405b9cd018a44221b5ec35b1aa69ada73f724
William Busby <williambusby1999@gmail.com>
2022-03-04 16:20:41 +0000

    Completed tests on currencycard and recipientcard

--- src/components/MakeTransferPages/MakeTransferCurrencyCard/MakeTransferCurrencyCard.jsx
+++ src/components/MakeTransferPages/MakeTransferCurrencyCard/MakeTransferCurrencyCard.jsx
@@ -9,13 +9,13 @@ const MakeTransferCurrencyCard = (props) => {
   return (
     <div className="transfer-page__currency__card">
       <div className="transfer-page__currency__card--left">
-        <CurrencyFlag currency={currency.currencyCode} width={40} onClick={handleChangingCurrency} className="transfer-page__currency__card__flag" />
+        <CurrencyFlag currency={currency.currencyCode} width={40} onClick={handleChangingCurrency} className="transfer-page__currency__card__flag" data-testid={"currency-flag"}/>
         <p>{currency.currencyName}</p>
       </div>
       <div className="transfer-page__currency__card--right">
         <p className="transfer-page__currency__card__account-number">{currency.liveRate.toFixed(4)}</p>
       </div>   
-      <div className="transfer-page__currency__card__overlay" onClick={handleChangingCurrency} id={`transfer-page__currency__${currency.currencyCode}`}></div>
+      <div className="transfer-page__currency__card__overlay" onClick={handleChangingCurrency} id={`transfer-page__currency__${currency.currencyCode}`} data-testid="overlay"></div>
     </div>
   )
 }
