c3383541b9d18a5db67d4b8801e5047bd77f42b6
Ebrima Sallah <sallahebrima@ymail.com>
2022-03-09 08:18:45 +0000

    refactored the testing on dropdown

--- src/components/DropDown/DropDown.test.js
+++ src/components/DropDown/DropDown.test.js
@@ -1,13 +1,13 @@
-import { render, screen, fireEvent } from '@testing-library/react';
+import { render, screen, fireEvent, waitFor, findByText } from '@testing-library/react';
 import DropDown from "./DropDown.jsx";
 
 const codes = ["usd", "gbp", "eur"];
 
 describe ("Testing the drop-down menu item", ()=> {
-  test("It should render", ()=> {
-    //Arrange 
-    render(<DropDown codes={codes}/>)
 
+  test("It should render", ()=> { 
+    //Arrange 
+    render(<DropDown codes={codes} />)
     //Act 
     const menu = screen.getByTestId("currency-selector");
     //Assert
@@ -16,30 +16,40 @@ describe ("Testing the drop-down menu item", ()=> {
   })
  
   test('should render input placeholder without errors', () => {
+    //Arrange 
     render(<DropDown codes={codes}/>);
-
+    //Act 
     const placeholder = screen.getByText('Select...');
-
+    //Assert
     expect(placeholder).toBeTruthy();
   });
 
 test('should render input option when changed', () => {
-
   // Arrange 
   render(<DropDown codes={codes}/>);
-   
   // Act
   const items = screen.getByRole('combobox', {name: ""});
-
+  // const menu = screen.getByTestId("currency-selector");
   // Assert 
   expect(items).toHaveValue("");
-  
-  //Act 
-  fireEvent.change(items,{ target: { value: "usd" }});
 
-  //Assert
+  //ACT
+  fireEvent.change(items,{ target: { value: "usd" }});
+  // //Assert
   expect(items.getAttribute("value")).toBe("usd");
+  
 });
  
 })
 
+ // funstion to be tested on currency converted page
+// const handleChange = jest.fn();
+// expect(handleChange).toHaveBeenCalledTimes(0);
+  
+  //Act 
+  // eslint-disable-next-line testing-library/no-node-access
+  // fireEvent.keyDown(menu.firstChild, { key: 'ArrowDown' });
+  // eslint-disable-next-line testing-library/await-async-utils
+  // await screen.findByText('USD');
+  // fireEvent.click(screen.getByText('USD'));
+  // expect(handleChange).toHaveBeenCalledTimes(1);
