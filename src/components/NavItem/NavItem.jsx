36da06ae4b154e411b6be88f8ea02a13f9fda015
William Busby <williambusby1999@gmail.com>
2022-03-11 22:57:37 +0000

    Worked on routing w/ active navbar

--- src/components/NavItem/NavItem.jsx
+++ src/components/NavItem/NavItem.jsx
@@ -19,12 +19,15 @@ const iconListRegular = Object
 library.add(...iconListRegular)
 
 const NavItem = (props) => {
+
+  const pathLocation = window.location.pathname.split("/")[1].toLowerCase();
+  const propsLabel = props.label.toLowerCase().split(" ").join("");
   let className = 'navbar__nav-item';
-  if (props.activeButton === props.label) {
+  if (props.activeButton === props.label || pathLocation===propsLabel) {
     className += " navbar__nav-item--active"
   }
   return (
-    <Link to={"/" + props.label.split(" ").join("")} style={{ textDecoration: 'none' }} >
+    <Link to={"/" + propsLabel} style={{ textDecoration: 'none' }} >
       <div className={className} onClick={() => props.handleClick(props.label) } key={props.index} data-testid="navItem">
         <div className='navbar__nav-item--icon'>{props.icon}</div>
         <p>{props.label}</p>
