0cd0e1aed39230b8e96a44f95fdaa1b0658ddf88
William Busby <williambusby1999@gmail.com>
2022-03-13 11:18:59 +0000

    removed unneeded import

--- src/components/MakeTransferPages/MakeTransferForm/MakeTransferForm.test.js
+++ src/components/MakeTransferPages/MakeTransferForm/MakeTransferForm.test.js
@@ -1,5 +1,4 @@
 import MakeTransferForm from "./MakeTransferForm";
-import React, { useState } from "react";
 import { render, screen } from "@testing-library/react";
 import userEvent from "@testing-library/user-event";
 
