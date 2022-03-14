28cb37e3b8f6f1bdacab6da6ddbf3548a172a402
Lana Ferrari <ferrarirangel@gmail.com>
2022-03-14 13:27:30 +0000

    Fix background

--- src/containers/HomePage/HomePage.jsx
+++ src/containers/HomePage/HomePage.jsx
@@ -11,7 +11,7 @@ import HeaderHome from "../../components/HomePageComponents/HeaderHome/HeaderHom
 
 const HomePage = () => {
   return (
-    <div className="home">
+    <div>
       <HeaderHome />
       <main>
         <Home />
