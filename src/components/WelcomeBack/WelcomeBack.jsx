d7ad316af93be95975795a8417cd7d14c6948fed
Naib Rahman <naib.rahman@gmail.com>
2022-03-16 17:05:02 +0000

    Styling for welcome back component

--- src/components/WelcomeBack/WelcomeBack.jsx
+++ src/components/WelcomeBack/WelcomeBack.jsx
@@ -23,33 +23,31 @@ const WelcomeBack = () => {
 
   return (
     <form className="loginform" onSubmit={handleSubmit(onSubmit)}>
-        <div>
-            <img src={logo} alt="" />
-            <p>Synergy</p>
+        <div className="loginform__brand">
+            <img className="loginform__brand__logo" src={logo} alt="" />
+            <p className="loginform__brand__name" >Venturist</p>
         </div>
-          <div>
+          <div className="loginform__header">
               <h1>Welcome Back!</h1>
               <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
           </div>
-          <div>
+          <div className="loginform__email">
             <label>Email</label>
             <input type="text" {...register("email")} />
             {/* {errors.email && <p>{errors.email.message}</p>} */}
           </div>
-          <div>
+          <div className="loginform__password">
             <label>Password</label>
             <input type="text" {...register("password")} />
             {/* {errors.password && <p>{errors.password.message}</p>} */}
           </div>
           <div className="loginform__links">
-            <div>
+            <div className="loginform__links__signup" >
                 <p>Don't have an account? </p>
                 <Button buttonName="Sign Up" buttonStyle="clear"/>
             </div>
             <Button buttonName="Forgotten Password?" buttonStyle="clear"/>
           </div>
-         
-
       <Button buttonName="Login" buttonFunction={handleSubmit(onSubmit)} />
     </form>
   );
