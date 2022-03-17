604401217e1376d87f44cda6c0907771271ed1e7
Allan Shrestha <stevevai111@hotmail.com>
2022-03-17 21:41:27 +0000

    All conflicts fixed

--- src/components/HomePageComponents/Footer/Footer.jsx
+++ src/components/HomePageComponents/Footer/Footer.jsx
@@ -2,10 +2,7 @@ import React from "react";
 import "./Footer.scss";
 import logo from "../../../assets/logos/logo.png";
 import Button from "../../Button/Button";
-<<<<<<< HEAD
-=======
 import { Link } from "react-router-dom";
->>>>>>> 31a0eb8d968f7a7a74ce94b41b2f96eefbb3a3e6
 
 const Footer = () => {
   return (
@@ -14,13 +11,9 @@ const Footer = () => {
         <img src={logo} alt="Venturist" className="image" />
         <h2>Venturist</h2>
       </div>
-<<<<<<< HEAD
-      <Button buttonName="Sign up" />
-=======
       <Link to="/wallet">
         <Button buttonName="Sign up" />
       </Link>
->>>>>>> 31a0eb8d968f7a7a74ce94b41b2f96eefbb3a3e6
     </div>
   );
 };
--- src/components/HomePageComponents/Home/Home.jsx
+++ src/components/HomePageComponents/Home/Home.jsx
@@ -4,15 +4,8 @@ import img from "../../../assets/images/liveratescreenshot.png";
 
 const Home = () => {
   return (
-<<<<<<< HEAD
-    <section className="home">
-      <h1 className="home__title">Make your money go places.</h1>
-=======
     <section className="home" data-testid="home">
-      <h1 className="home__title">
-        Lorem ipsum dolor sit amet consectetur, adipisicing elit.
-      </h1>
->>>>>>> 31a0eb8d968f7a7a74ce94b41b2f96eefbb3a3e6
+      <h1 className="home__title">Make your money go places.</h1>
       <p className="home__p">
         Send money cheaper and easier than old-school banks. Send money at the
         real exchange rate with no hidden fees.
