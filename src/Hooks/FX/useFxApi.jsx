3cdf5e60c91b104691c641ae08b7beb0725509cc
= <=>
2022-03-22 14:53:40 +0000

    add some state inside of hook

--- src/Hooks/FX/useFxApi.jsx
+++ src/Hooks/FX/useFxApi.jsx
@@ -1,7 +1,12 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 
 const useFxApi = () => {
-  return <div>useFxApi</div>;
+  const [isLoaded, setIsLoaded] = useState(false);
+  const [data, setData] = useState(null);
+  const [serverError, setServerError] = useState(null);
+
+  return { isLoaded, data, serverError };
 };
 
 export default useFxApi;
