2eb314980321f07fc89fea53a3f5993f94f39386
= <=>
2022-03-22 15:12:25 +0000

    create basic hook funcitonality with axios

--- src/Hooks/FX/useFxApi.jsx
+++ src/Hooks/FX/useFxApi.jsx
@@ -1,12 +1,28 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
 
-const useFxApi = () => {
-  const [isLoaded, setIsLoaded] = useState(false);
+const useFxApi = (url) => {
+  const [loading, setIsLoading] = useState(false);
   const [data, setData] = useState(null);
   const [serverError, setServerError] = useState(null);
 
-  return { isLoaded, data, serverError };
+  useEffect(() => {
+    setIsLoading(true);
+    const fetchData = async () => {
+      try {
+        const response = await axios.get(url);
+        const data = await response?.data;
+        setData(data);
+        setIsLoading(false);
+      } catch (error) {
+        setServerError(error);
+        setIsLoading(false);
+      }
+    };
+    fetchData();
+  }, [url]);
+
+  return { loading, data, serverError };
 };
 
 export default useFxApi;
