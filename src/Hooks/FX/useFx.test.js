c934d30eb6f219107c1b644a75317007a7f08b41
= <=>
2022-03-23 12:34:09 +0000

    update hook to use state for URL rather than a param

--- src/Hooks/FX/useFx.test.js
+++ src/Hooks/FX/useFx.test.js
@@ -1,6 +1,35 @@
-import { render, screen } from "@testing-library/react";
 import useFxApi from "./useFxApi";
+import mockAxios from "./axios";
+import { renderHook } from "@testing-library/react-hooks";
+import { act } from "react-test-renderer";
 
-describe("should fetch and return data and status", () => {
-  test("", () => {});
+const BASE_URL = "https://jsonplaceholder.typicode.com/users";
+
+describe("fetchUsers", () => {
+  afterEach(() => {
+    mockAxios.reset();
+  });
+
+  describe("when API call is successful", () => {
+    it("should return users list", async () => {
+      // given
+      const users = [
+        { id: 1, name: "John" },
+        { id: 2, name: "Andrew" },
+      ];
+      mockAxios.get.mockResolvedValueOnce(users);
+
+      // when
+      const { loading, data, serverError } = renderHook(() =>
+        useFxApi(BASE_URL)
+      );
+
+      //   await act(async () => );
+
+      expect(loading).toBe(false);
+
+      // then
+      //expect(mockAxios.get).toHaveBeenCalledWith(`${BASE_URL}/users`);
+    });
+  });
 });
--- src/Hooks/FX/useFxApi.jsx
+++ src/Hooks/FX/useFxApi.jsx
@@ -1,28 +1,36 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useRef } from "react";
 import axios from "axios";
 
-const useFxApi = (url) => {
+const useFxApi = () => {
+  const firstUpdate = useRef(true);
   const [loading, setIsLoading] = useState(false);
   const [data, setData] = useState(null);
   const [serverError, setServerError] = useState(null);
+  const [apiUrl, seApiUrl] = useState("";)
+
+  const fetchData = async () => {
+    try {
+      const response = await axios.get(url);
+      const data = await response?.data;
+      setData(data);
+      setIsLoading(false);
+    } catch (error) {
+      setServerError(error);
+      setIsLoading(false);
+    }
+  };
 
   useEffect(() => {
     setIsLoading(true);
-    const fetchData = async () => {
-      try {
-        const response = await axios.get(url);
-        const data = await response?.data;
-        setData(data);
-        setIsLoading(false);
-      } catch (error) {
-        setServerError(error);
-        setIsLoading(false);
-      }
-    };
+    if(!firstUpdate.current){
+      fetchData()
+    }
+    firstUpdate.current = false;
+    
     fetchData();
-  }, [url]);
+  }, [apiUrl]);
 
-  return { loading, data, serverError };
+  return { loading, data, serverError, callApi: seApiUrl };
 };
 
 export default useFxApi;
