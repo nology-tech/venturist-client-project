e433242f8925967384d120f488ece800caaede17
William Busby <williambusby1999@gmail.com>
2022-03-23 18:06:47 +0000

    Added yup form validation rather than if

--- src/components/MakeTransferPages/MakeTransferAddRecipient/MakeTransferAddRecipient.jsx
+++ src/components/MakeTransferPages/MakeTransferAddRecipient/MakeTransferAddRecipient.jsx
@@ -1,50 +1,53 @@
-import React, { useState } from 'react';
+import React from 'react';
 import "./MakeTransferAddRecipient.scss";
+import { useForm } from "react-hook-form";
 import cross from "./../../../assets/icons/black-cross.png"
 import Button from "./../../Button/Button";
+import { yupResolver } from "@hookform/resolvers/yup";
+import * as yup from "yup";
+
+const schema = yup.object({
+  name: yup.string().max(35).required("Recipient Name is required"),
+  type: yup.string().max(35).required("Account Type is required"),
+  accountNumber: yup
+    .number()
+    .test(
+      "len",
+      "Must contain between 8 and 11 numbers",
+      (val) => val.toString().length >= 8 && val.toString().length <= 11
+    )
+    .typeError("Must contain between 8 and 11 numbers")
+    .required("Account Number is required"),
+  sortCode: yup
+    .number()
+    .test(
+      "len",
+      "Must be exactly 6 characters",
+      (val) => val.toString().length === 6
+    )
+    .typeError("Must contain 6 numbers")
+    .required("Sort Code is required"),
+});
 
 const MakeTransferAddRecipient = (props) => {
 
   const {toggleAddRecipient, exchangeInfo, setExchangeInfo, handleShowConfirmation} = props;
 
-  const [name, setName] = useState("");
-  const [type, setType] = useState("");
-  const [accountNumber, setAccountNumber] = useState("");
-  const [sortCode, setSortCode] = useState("");
-
-  const handleName = (event) => {
-    setName(event.target.value);
-  };
-
-  const handleType = (event) => {
-    setType(event.target.value);
-  };
-
-  const handleNumber = (event) => {
-    setAccountNumber(event.target.value);
-  };
-
-  const handleSort = (event) => {
-    setSortCode(event.target.value);
-  };
+  const { register, handleSubmit, formState: { errors }} = useForm({resolver: yupResolver(schema)});
 
   const handleCancel = () => {
     toggleAddRecipient();
   };
   
-  const handleAddRecipient = () => {
-    if(type !== "" && name !== "" && sortCode.length === 6 && !isNaN(sortCode) && (accountNumber.length >= 8 && accountNumber.length <= 11) && !isNaN(accountNumber)) {
-      setExchangeInfo({...exchangeInfo}, exchangeInfo.exchangeTo.user = {
-        firstName: name,
-        type: type,
-        accountNumber: accountNumber,
-        sortCode: sortCode
-      });
-      toggleAddRecipient();
-      handleShowConfirmation();
-    } else {
-      alert("Please input valid parameters.");
-    }
+  const handleAddRecipient = (data) => {
+    setExchangeInfo({...exchangeInfo}, exchangeInfo.exchangeTo.user = {
+      firstName: data.name,
+      type: data.type,
+      accountNumber: data.accountNumber,
+      sortCode: data.sortCode
+    });
+    toggleAddRecipient();
+    handleShowConfirmation();
   };
 
   return (
@@ -52,19 +55,23 @@ const MakeTransferAddRecipient = (props) => {
     <div className="transfer-page__add-recipient" data-testid="add-recipient">
       <h2 className="transfer-page__add-recipient__header">Add Recipient</h2>
       <img src={cross} alt="Close menu" className="transfer-page__add-recipient__close-menu" onClick={handleCancel}/>
-      <div className="transfer-page__add-recipient__content">
+      <form className="transfer-page__add-recipient__content">
         <label htmlFor="name" className="transfer-page__add-recipient__label">Recipient Name</label>
-        <input type="text" name="name" className="transfer-page__add-recipient__input" onChange={handleName} required ></input>
+        <input {...register("name")} className="transfer-page__add-recipient__input" type="text" />
+        <p className="transfer-page__add-recipient__error">{errors.name?.message}</p>
         <label htmlFor="accountType" className="transfer-page__add-recipient__label">Account Type</label>
-        <input type="text" name="accountType" className="transfer-page__add-recipient__input" onChange={handleType} required ></input>
+        <input {...register("type")} className="transfer-page__add-recipient__input" type="text" />
+        <p className="transfer-page__add-recipient__error">{errors.type?.message}</p>
         <label htmlFor="accountNumber" className="transfer-page__add-recipient__label">Account Number</label>
-        <input type="text" name="accountNumber" className="transfer-page__add-recipient__input" onChange={handleNumber} required ></input>
+        <input {...register("accountNumber")} className="transfer-page__add-recipient__input" type="text" />
+        <p className="transfer-page__add-recipient__error">{errors.accountNumber?.message}</p>
         <label htmlFor="sort" className="transfer-page__add-recipient__label">Sort Code</label>
-        <input type="text" name="sort" className="transfer-page__add-recipient__input" onChange={handleSort} required ></input>
-      </div>
+        <input {...register("sortCode")} className="transfer-page__add-recipient__input" type="text" />
+        <p className="transfer-page__add-recipient__error">{errors.sortCode?.message}</p>
+      </form>
       <div className="transfer-page__add-recipient__buttons">
         <Button buttonName="Cancel" buttonStyle="clear" buttonFunction={handleCancel} />
-        <Button buttonName="Continue" buttonFunction={handleAddRecipient} />
+        <Button buttonName="Continue" buttonFunction={handleSubmit(handleAddRecipient)} />
       </div>
     </div>
     <div className="overlay-background"></div>
