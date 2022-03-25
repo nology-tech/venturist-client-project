8622a111b07f0afe709e78be6b6f62584ffd49ab
= <=>
2022-03-25 12:20:08 +0000

    remove unused vars for build

--- src/containers/LiveRates/LiveRates.jsx
+++ src/containers/LiveRates/LiveRates.jsx
@@ -29,6 +29,7 @@ const LiveRates = (props) => {
         console.log(err);
       }
     }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [status, editBaseCurrency]);
 
   const handleAddCurrency = (value) => {
--- src/containers/MakeTransferPage/MakeTransferPage.jsx
+++ src/containers/MakeTransferPage/MakeTransferPage.jsx
@@ -5,41 +5,53 @@ import Header from "../../components/Header/Header";
 import MakeTransferConfirmAccount from "../../components/MakeTransferPages/MakeTransferConfirmAccount/MakeTransferConfirmAccount";
 import MakeTransferForm from "../../components/MakeTransferPages/MakeTransferForm/MakeTransferForm";
 import MakeTransferConfirmation from "../../components/MakeTransferPages/MakeTransferConfirmation/MakeTransferConfirmation";
-import { useParams} from "react-router-dom";
+//import { useParams} from "react-router-dom";
 
 const MakeTransferPage = (props) => {
   const { liveRateData, profileData, contactData } = props;
-
 
   const exchangeBase = {
     exchangeFrom: {
       user: profileData,
       currency: liveRateData[0],
       amount: 0,
-      fee: 0
+      fee: 0,
     },
     exchangeTo: {
       user: {},
       currency: liveRateData[1],
-      amount: 0
-    }
+      amount: 0,
+    },
   };
 
-  const [exchangeInfo, setExchangeInfo] = useState(exchangeBase); 
+  const [exchangeInfo, setExchangeInfo] = useState(exchangeBase);
 
-  const [showInitialForm, setShowInitialForm] = useState(true); 
-  const [showConfirmAccount, setShowConfirmAccount] = useState(false); 
+  const [showInitialForm, setShowInitialForm] = useState(true);
+  const [showConfirmAccount, setShowConfirmAccount] = useState(false);
   const [showConfirmation, setShowConfirmation] = useState(false);
 
   const handleShowForm = () => {
     const amountInput = document.getElementById("amountInput").value;
-    if(!(amountInput < exchangeInfo.exchangeFrom.user.holdings[exchangeInfo.exchangeFrom.currency.currencyCode])) {
+    if (
+      !(
+        amountInput <
+        exchangeInfo.exchangeFrom.user.holdings[
+          exchangeInfo.exchangeFrom.currency.currencyCode
+        ]
+      )
+    ) {
       alert("You don't have enough of that currency to send.");
       return;
     }
     if (amountInput.match(/^\d*(\.\d{0,2})?$/) && amountInput > 0) {
-      setExchangeInfo({...exchangeInfo}, exchangeInfo.exchangeFrom.amount=Number(amountInput).toFixed(2));
-      setExchangeInfo({...exchangeInfo}, exchangeInfo.exchangeFrom.fee=Number(amountInput*0.01).toFixed(2));
+      setExchangeInfo(
+        { ...exchangeInfo },
+        (exchangeInfo.exchangeFrom.amount = Number(amountInput).toFixed(2))
+      );
+      setExchangeInfo(
+        { ...exchangeInfo },
+        (exchangeInfo.exchangeFrom.fee = Number(amountInput * 0.01).toFixed(2))
+      );
       setShowInitialForm(false);
       setShowConfirmAccount(true);
     }
@@ -48,7 +60,7 @@ const MakeTransferPage = (props) => {
   const handleShowConfirmation = () => {
     setShowConfirmation(true);
     setShowConfirmAccount(false);
-  }
+  };
 
   return (
     <div className="make-transfer" data-testid="make-transfer">
@@ -60,26 +72,24 @@ const MakeTransferPage = (props) => {
 
       {showInitialForm && (
         <MakeTransferForm
-          exchangeInfo={exchangeInfo}  
-          setExchangeInfo={setExchangeInfo} 
-          handleShowForm={handleShowForm} 
-          liveRateData={liveRateData} 
+          exchangeInfo={exchangeInfo}
+          setExchangeInfo={setExchangeInfo}
+          handleShowForm={handleShowForm}
+          liveRateData={liveRateData}
         />
       )}
 
       {showConfirmAccount && (
-        <MakeTransferConfirmAccount 
-          exchangeInfo={exchangeInfo} 
-          setExchangeInfo={setExchangeInfo} 
-          data={contactData} 
+        <MakeTransferConfirmAccount
+          exchangeInfo={exchangeInfo}
+          setExchangeInfo={setExchangeInfo}
+          data={contactData}
           handleShowConfirmation={handleShowConfirmation}
         />
       )}
 
       {showConfirmation && (
-        <MakeTransferConfirmation 
-          exchangeInfo={exchangeInfo}
-        />
+        <MakeTransferConfirmation exchangeInfo={exchangeInfo} />
       )}
     </div>
   );
