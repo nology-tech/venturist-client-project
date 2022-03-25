4bb0879f9d684d7d34dcd01abdef4708eb61ae3e
Naib Rahman <naib.rahman@gmail.com>
2022-03-25 15:33:17 +0000

    render contacts list

--- src/containers/ContactsPage/ContactsPage.jsx
+++ src/containers/ContactsPage/ContactsPage.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import Header from '../../components/Header/Header';
+import ContactsList from '../ContactsList/ContactsList';
 import './ContactsPage.scss'
 
 const ContactsPage = () => {
   return (
     <div className='contacts-page'>
       <Header title="Contacts" pageFunctionHeading="Your Contacts" textDescription="View your contacts here." />
+      <ContactsList />
     </div>
   )
 }
