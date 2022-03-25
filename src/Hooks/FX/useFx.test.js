de288179af630766c88d9531c9749cbf50936e08
= <=>
2022-03-25 11:57:03 +0000

    remove breaking test dependency in useFxApi

--- src/Hooks/FX/useFx.test.js
+++ src/Hooks/FX/useFx.test.js
@@ -1,20 +1,18 @@
 import useFxApi from "./useFxApi";
 import { renderHook } from "@testing-library/react-hooks";
 import { rest } from "msw";
-import { setupServer } from "msw/node";
-import { render, fireEvent, waitFor, screen } from "@testing-library/react";
+//import { setupServer } from "msw/node";
 import "@testing-library/jest-dom";
-import { act } from "react-test-renderer";
 
-const server = setupServer(
-  rest.get("/greeting", (req, res, ctx) => {
-    return res(ctx.json({ greeting: "hello there" }));
-  })
-);
+// const server = setupServer(
+//   rest.get("/greeting", (req, res, ctx) => {
+//     return res(ctx.json({ greeting: "hello there" }));
+//   })
+// );
 
-beforeAll(() => server.listen());
-afterEach(() => server.resetHandlers());
-afterAll(() => server.close());
+// beforeAll(() => server.listen());
+// afterEach(() => server.resetHandlers());
+// afterAll(() => server.close());
 
 test("should return with default state", async () => {
   const { result } = renderHook(() => useFxApi(""));
