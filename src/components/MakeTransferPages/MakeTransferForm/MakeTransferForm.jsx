6776f0f573ef3e78da0a83b58d2ef7208a686785
harvir_g1@icloud.com <harvir_g1@icloud.com>
2022-03-29 16:21:57 +0100

    integrated all fx API into all relevant components

--- src/components/MakeTransferPages/MakeTransferForm/MakeTransferForm.jsx
+++ src/components/MakeTransferPages/MakeTransferForm/MakeTransferForm.jsx
@@ -1,18 +1,26 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import Button from "../../Button/Button";
 import "./MakeTransferForm.scss";
 import CurrencyFlag from "react-currency-flags";
 import MakeTransferChooseModal from "../MakeTransferChooseModal/MakeTransferChooseModal";
+import useFxApi from "../../../Hooks/FX/useFxApi";
 
 const MakeTransferForm = props => {
-  const { handleShowForm, liveRateData, exchangeInfo, setExchangeInfo } = props;
+  const { handleShowForm, liveRateData, exchangeInfo, setExchangeInfo, ratesArr } = props;
 
   const [exchangeAmount, setExchangeAmount] = useState(0);
   const [showCurrencyModal, setShowCurrencyModal] = useState(false); 
   const [changingCurrency, setChangingCurrency] = useState("to"); 
+  const [baseCurrency, setBaseCurrency] = useState("GBP");
+
+  const [baseAmount, setBaseAmount] = useState(1);
+  const [defaultCurrencies, setDefaultCurrencies] = useState(["USD", "EUR"]);
+  const [filteredRates, setFilteredRates] = useState([]);
+  const [message, setMessage] = useState("Loading live rates...");
 
   const exchangeFrom=exchangeInfo.exchangeFrom.currency;
   const exchangeTo=exchangeInfo.exchangeTo.currency;
+
 
   const handleShowCurrencyModal = () => {
     setShowCurrencyModal(!showCurrencyModal);
@@ -29,7 +37,7 @@ const MakeTransferForm = props => {
 
   const handleCurrency = (event) => {
     const chosenCurrencyCode = event.target.id.slice(21,24);
-    const chosenCurrencyObj = liveRateData.filter(currency => currency.currencyCode === chosenCurrencyCode)[0];
+    const chosenCurrencyObj = ratesArr.filter(currency => currency.currencyCode === chosenCurrencyCode)[0];
     if(changingCurrency==="to") {
       setExchangeInfo({...exchangeInfo},exchangeInfo.exchangeTo.currency=chosenCurrencyObj);
     } else if(changingCurrency==="from"){
@@ -37,6 +45,8 @@ const MakeTransferForm = props => {
     }
     handleShowCurrencyModal();
   };
+
+
 
   const calculateConversion = () => {
     return exchangeTo.liveRate / exchangeFrom.liveRate;
@@ -144,7 +154,7 @@ const MakeTransferForm = props => {
     </form>
     {showCurrencyModal && (
       <MakeTransferChooseModal type="Currency"
-        content={liveRateData}
+        content={ratesArr}
         handleEvent={handleCurrency}
         handleShowModal={handleShowCurrencyModal}
         handleSearch={()=>alert("Searching")}
--- src/containers/MakeTransferPage/MakeTransferPage.jsx
+++ src/containers/MakeTransferPage/MakeTransferPage.jsx
@@ -1,14 +1,36 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import "./MakeTransferPage.scss";
 
 import Header from "../../components/Header/Header";
 import MakeTransferConfirmAccount from "../../components/MakeTransferPages/MakeTransferConfirmAccount/MakeTransferConfirmAccount";
 import MakeTransferForm from "../../components/MakeTransferPages/MakeTransferForm/MakeTransferForm";
 import MakeTransferConfirmation from "../../components/MakeTransferPages/MakeTransferConfirmation/MakeTransferConfirmation";
+import useFxApi from "../../Hooks/FX/useFxApi";
 //import { useParams} from "react-router-dom";
 
 const MakeTransferPage = (props) => {
   const { liveRateData, profileData, contactData } = props;
+
+  const [baseCurrency, setBaseCurrency] = useState("GBP");
+
+  const url = `https://venturist-app.nw.r.appspot.com/currencies/${baseCurrency}`;
+
+  const { data, status, ratesArr, getData } = useFxApi();
+
+  const [showDropDown, setShowDropDown] = useState(false);
+  const [editBaseCurrency, setEditBaseCurrency] = useState(false);
+  const [baseAmount, setBaseAmount] = useState(1);
+  const [defaultCurrencies, setDefaultCurrencies] = useState(["USD", "EUR"]);
+  const [filteredRates, setFilteredRates] = useState([]);
+  const [message, setMessage] = useState("Loading live rates...");
+
+  console.log(data);
+  // console.log(data.rates["GBP"])
+
+  if(data) {
+    console.log(data.rates["USD"])
+  }
+
 
   const exchangeBase = {
     exchangeFrom: {
@@ -19,10 +41,29 @@ const MakeTransferPage = (props) => {
     },
     exchangeTo: {
       user: {},
-      currency: liveRateData[1],
+      currency: liveRateData[0],
       amount: 0,
     },
   };
+  
+  useEffect(() => {
+    getData(url);
+    if (status === "success") {
+      try {
+        setFilteredRates(filterRates());
+      } catch (err) {
+        setMessage("Error getting rates. Please try again later");
+      }
+    }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [status]);
+
+  const filterRates = () => {
+    return ratesArr.filter((item) =>
+      defaultCurrencies.includes(item.currencyCode)
+    );
+  };
+
 
   const [exchangeInfo, setExchangeInfo] = useState(exchangeBase);
 
@@ -75,7 +116,7 @@ const MakeTransferPage = (props) => {
           exchangeInfo={exchangeInfo}
           setExchangeInfo={setExchangeInfo}
           handleShowForm={handleShowForm}
-          liveRateData={liveRateData}
+          ratesArr={ratesArr}
         />
       )}
 
