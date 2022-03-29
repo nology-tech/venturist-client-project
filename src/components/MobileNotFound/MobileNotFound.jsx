66921124caa4d1b8dc21c30e6f946839f62a5f39
William Busby <williambusby1999@gmail.com>
2022-03-29 15:05:14 +0100

    Added test

--- src/components/MobileNotFound/MobileNotFound.jsx
+++ src/components/MobileNotFound/MobileNotFound.jsx
@@ -5,7 +5,7 @@ import "./MobileNotFound.scss";
 
 const MobileNotFound = () => {
   return (
-    <div className="mobile-empty">
+    <div className="mobile-empty" data-testid="mobile-empty">
       <h2 className="mobile-empty__header">Oops!</h2>
       <p className="mobile-empty__text">That feature is not currently available on mobile.</p>
       <p className="mobile-empty__text">Sorry for any inconvenience caused.</p>
--- src/components/MobileNotFound/MobileNotFound.test.js
+++ src/components/MobileNotFound/MobileNotFound.test.js
@@ -1,5 +1,13 @@
 import { render, screen, fireEvent } from "@testing-library/react";
+import MobileNotFound from "./MobileNotFound";
+import {BrowserRouter as Router} from "react-router-dom";
 
 describe('Render tests', () => {
-  test('Component renders', () => {});
+  test('Component renders', () => {
+    render(<Router><MobileNotFound /></Router>);
+
+    const component = screen.getByTestId("mobile-empty");
+    
+    expect(component).toBeInTheDocument();
+  });
 });
